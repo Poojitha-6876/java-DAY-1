038061edaf5f9d1eba4a4f8772926649d83728d6
Poojitha-6876 <poojithareddy654@gmail.com>
2026-02-28 11:21:26 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,25 @@
+class Pattern1{
+    public static void main(String[] args)
+    {
+        int i,j;
+        int n=5;
+        for(i=1;i<=5;i++)
+        {
+            for(j=1;j<=5;j++)
+            {
+                if(i==1||i==n||j==1||j==n)
+                    {
+                  System.out.print("*");
+                  {
+                    else
+                    {
+                        System.out.println(" ");
+                        {
+
+                        }
+                    }
+                  }
+                }
+            }
+        }
+    }
